b3a83bd9e6c998600b533097defd51b4c231d3d8
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-03-26 09:00:26 +0000

    sim: Remove flash_area_sector_from_off function
    This function is not used anywhere in the tree, remove it
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- sim/mcuboot-sys/csupport/storage/flash_map.h
+++ sim/mcuboot-sys/csupport/storage/flash_map.h
@@ -136,13 +136,6 @@ uint8_t flash_area_erased_val(const struct flash_area *);
 int flash_area_get_sectors(int fa_id, uint32_t *count,
   struct flash_sector *sectors);
 
-
-/* Retrieve the flash sector a given offset belongs to.
- *
- * Returns 0 on success, or an error code on failure.
- */
-int flash_area_sector_from_off(uint32_t off, struct flash_sector *sector);
-
 /* Retrieve the flash sector a given offset, within flash area.
  *
  * @param fa        flash area.
